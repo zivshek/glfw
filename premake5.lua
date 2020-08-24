3dab2bc1ad4b2bc94daea93f2ba4a55b6ab54da4
Hao Shi <ziv.hao.shek@gmail.com>
2020-08-23 23:01:23 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,13 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-    
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+    files
+    {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
@@ -17,12 +18,33 @@ project "GLFW"
         "src/vulkan.c",
         "src/window.c"
     }
-    
-	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
+    filter "system:linux"
+        pic "On"
+
         systemversion "latest"
-        staticruntime "On"
-        
+
+        files
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
+
+        defines
+        {
+            "_GLFW_X11"
+        }
+
+    filter "system:windows"
+        systemversion "latest"
+
         files
         {
             "src/win32_init.c",
@@ -36,10 +58,16 @@ project "GLFW"
             "src/osmesa_context.c"
         }
 
-		defines 
-		{ 
+        defines 
+        { 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+        }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
